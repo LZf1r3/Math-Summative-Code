13546a43c85f03fbf9346018a20318c7ba66f28e
LZf1r3 <lucas.mfaiad@gmail.com>
2023-03-08 20:53:01 -0800

    I created a code for my Math Summative work.
    In the 'Codigo Final.py' I mixed all of the other codes and I made the project to resolve some problems about pricing

---
+++ Codigo Base.py
@@ -0,0 +1,49 @@
+#codigo do Math Summative work
+
+#precos do ticket unico com cartao de credito/debito
+tickets_unicos_cartao_zona_1 = 3.10
+tickets_unicos_cartao_zona_2 = 4.45
+tickets_unicos_cartao_soma = tickets_unicos_cartao_zona_1 + tickets_unicos_cartao_zona_2
+
+#precos do ticket unico com cartao compass
+tickets_unicos_compass_zona_1 = 2.50
+tickets_unicos_compass_zona_2 = 3.65
+tickets_unicos_compass_soma = tickets_unicos_compass_zona_1 + tickets_unicos_compass_zona_2
+
+#precos dia todo
+tickets_dia_todas_as_zonas = 11
+
+#precos passe mensal
+tickets_mes_zona_1 = 102.55
+tickets_mes_zona_2 = 137.10
+tickets_mes_soma = tickets_mes_zona_1 + tickets_mes_zona_2
+
+#variaveis do codigo
+dias = 31
+feriados = 0
+dias_doente = 0
+
+#comeco do codigo
+print("Bem vindo ao programa de precos LZ! Por gentileza responda as seguintes perguntas com precisao: ")
+
+#Feriados / finais de semana
+feriados = int(input("Por gentileza digite apenas o numero de dias que serao feriados e finais de semana: "))
+dias_doente = int(input("Digite quantos dias da semana voce podera ficar doente: "))
+dias_sem_aula = feriados + dias_doente
+dias = dias - dias_sem_aula
+
+#calculos de preco:
+preco_mensal_ticket_unico_cartao = "{:.2f}".format(tickets_unicos_cartao_soma * dias)
+
+preco_mensal_ticket_unico_compass = "{:.2f}".format(tickets_unicos_compass_soma * dias)
+
+preco_mensal_ticket_dia_todas_as_zonas = "{:.2f}".format(tickets_dia_todas_as_zonas * dias)
+
+preco_mensal_ticket_mes = "{:.2f}".format(tickets_mes_soma)
+
+#enviando os arquivos para a tela de resposta
+print("O valor do ticket unico com cartao de credito ou debito no periodo de um mes sera de ${}".format(preco_mensal_ticket_unico_cartao))
+print("O valor do ticket unico com o cartao compass no periodo de um mes sera de ${}".format(preco_mensal_ticket_unico_compass))
+print("O valor do ticket 'All Day' no periodo de um mes sera de ${}".format(preco_mensal_ticket_dia_todas_as_zonas))
+print("O valor do ticket mensal das duas zonas no periodo de um mes sera de ${}".format(preco_mensal_ticket_mes))
+print("Obrigado por usar o sistema LZ! Volte sempre.")
---
+++ Codigo Final.py
@@ -0,0 +1,115 @@
+    #codigo do Math Summative work
+
+#comeco do codigo
+lingua = str(input("Write 'English' to set the language to English or 'Portuguese to set the language to Portuguese: "))
+
+def Portuguese():
+    #comecando o codigo
+    print("Bem vindo ao programa de precos LZ! Por gentileza responda as seguintes perguntas com precisao: \n")
+
+    #precos do ticket unico com cartao de credito/debito
+    tickets_unicos_cartao_zona_1 = 3.10
+    tickets_unicos_cartao_zona_2 = 4.45
+    tickets_unicos_cartao_soma = tickets_unicos_cartao_zona_1 + tickets_unicos_cartao_zona_2
+
+    #precos do ticket unico com cartao compass
+    tickets_unicos_compass_zona_1 = 2.50
+    tickets_unicos_compass_zona_2 = 3.65
+    tickets_unicos_compass_soma = tickets_unicos_compass_zona_1 + tickets_unicos_compass_zona_2
+
+    #precos dia todo
+    tickets_dia_todas_as_zonas = 11
+
+    #precos passe mensal
+    tickets_mes_zona_1 = 102.55
+    tickets_mes_zona_2 = 137.10
+    tickets_mes_soma = tickets_mes_zona_1 + tickets_mes_zona_2
+
+    #variaveis do codigo
+    dias = 31
+    feriados = 0
+    dias_doente = 0
+
+    #Feriados / finais de semana
+    feriados = int(input("Por gentileza digite apenas o numero de dias que serao feriados e finais de semana: "))
+    dias_doente = int(input("Digite quantos dias da semana voce podera ficar doente: "))
+    dias_sem_aula = feriados + dias_doente
+    dias = dias - dias_sem_aula
+
+    #calculos de preco:
+    preco_mensal_ticket_unico_cartao = "{:.2f}".format((tickets_unicos_cartao_soma * dias)*2)
+
+    preco_mensal_ticket_unico_compass = "{:.2f}".format((tickets_unicos_compass_soma * dias)*2)
+
+    preco_mensal_ticket_dia_todas_as_zonas = "{:.2f}".format(tickets_dia_todas_as_zonas * dias)
+
+    preco_mensal_ticket_mes = "{:.2f}".format(tickets_mes_soma)
+
+    #enviando os arquivos para a tela de resposta
+    print("O valor do ticket unico com cartao de credito ou debito no periodo de um mes sera de $\n{}".format(preco_mensal_ticket_unico_cartao))
+    print("O valor do ticket unico com o cartao compass no periodo de um mes sera de ${}\n".format(preco_mensal_ticket_unico_compass))
+    print("O valor do ticket 'All Day' no periodo de um mes sera de ${}\n".format(preco_mensal_ticket_dia_todas_as_zonas))
+    print("O valor do ticket mensal das duas zonas no periodo de um mes sera de ${}\n".format(preco_mensal_ticket_mes))
+    print("Obrigado por usar o sistema LZ! Volte sempre.")
+
+#codigo em ingles
+def English():
+
+    #comecando o codigo
+    print("Welcome to LZ's services! Please, answer the questions below with precision: \n")
+    
+    #precos do ticket unico com cartao de credito/debito
+    tickets_unicos_cartao_zona_1 = 3.10
+    tickets_unicos_cartao_zona_2 = 4.45
+    tickets_unicos_cartao_soma = tickets_unicos_cartao_zona_1 + tickets_unicos_cartao_zona_2
+
+    #precos do ticket unico com cartao compass
+    tickets_unicos_compass_zona_1 = 2.50
+    tickets_unicos_compass_zona_2 = 3.65
+    tickets_unicos_compass_soma = tickets_unicos_compass_zona_1 + tickets_unicos_compass_zona_2
+
+    #precos dia todo
+    tickets_dia_todas_as_zonas = 11
+
+    #precos passe mensal
+    tickets_mes_zona_1 = 102.55
+    tickets_mes_zona_2 = 137.10
+    tickets_mes_soma = tickets_mes_zona_1 + tickets_mes_zona_2
+
+    #variaveis do codigo
+    dias = 31
+    feriados = 0
+    dias_doente = 0
+
+    #Feriados / finais de semana
+    feriados = int(input("Please, type the numbers of days that you won't use the transport: (It includes Weekends and Holidays only!) \n"))
+    dias_doente = int(input("Please, type the numbers of days that you think are necessary for any problem:  \n"))
+    dias_sem_aula = feriados + dias_doente
+    dias = dias - dias_sem_aula
+
+    #calculos de preco:
+    preco_mensal_ticket_unico_cartao = "{:.2f}".format((tickets_unicos_cartao_soma * dias)*2)
+
+    preco_mensal_ticket_unico_compass = "{:.2f}".format((tickets_unicos_compass_soma * dias)*2)
+
+    preco_mensal_ticket_dia_todas_as_zonas = "{:.2f}".format(tickets_dia_todas_as_zonas * dias)
+
+    preco_mensal_ticket_mes = "{:.2f}".format(tickets_mes_soma)
+
+    #enviando os arquivos para a tela de resposta
+    print("---------------------------------------------------------------")
+    print("If you choose to buy the single ticket for zone '1' and '2', and pay with credit/debit card, the price per month would be ${}\n".format(preco_mensal_ticket_unico_cartao))
+    print("If you choose to buy the single ticket for zone '1' and '2', and pay with the compass card, the price per month would be ${}\n".format(preco_mensal_ticket_unico_compass))
+    print("If you choose to buy the 'All Day' ticket, the price per month would be {}\n".format(preco_mensal_ticket_dia_todas_as_zonas))
+    print("If you choose to buy the whole month ticket, the price would be ${}\n".format(preco_mensal_ticket_mes))
+    print("Thank you for using LZ's services. We'll wait for you!")
+
+#selecionando lingua
+if lingua == "Portuguese" or lingua == "portuguese" or lingua == "portugues":
+    print("Idioma selecionado como 'Portugues'! \n \n \n")
+    Portuguese()
+elif lingua == "English" or lingua == "english" or lingua =="ingles":
+    print("Language setted 'English'!  \n \n \n")
+    English()
+else:
+    print("Comando nao encontrado! / Command not found!")
